93b65b6530ace36a8342121123a374050f4dec81
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2018-10-04 09:50:15 +0300

    xd

---
+++ WeatherApp.Core/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string City)
+        {
+            string key = "1dad50e251b03e9137b8b650a95bc9ff";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q="+ City + "units=metric&appid=" + key;
+
+            dynamic results = await DataService.GetDataFromService(queryString).ConfigureAwait(false);
+            var weather = new Weather();
+            weather.Title = (string)results["name"];
+            weather.Temperature = (string)results["main"]["temp"] + " C";
+            weather.WindSpeed = (string)results["wind"]["speed"] + " m/s";
+            weather.AirPressure = (string)results["Main"]["pressure"] + "hpa"; 
+            return weather;
+        }
+    }
+}
---
+++ WeatherApp/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace WeatherApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var SearchButton = FindViewById<Button>(Resource.Id.button1);
+            var City = FindViewById<EditText>(Resource.Id.citySearch);
+
+            SearchButton.Click += Button_Click;
+
+            
+        }
+        private async void Button_Click(object sender, System.EventArgs e)
+        {
+            var City = FindViewById<TextView>(Resource.Id.City);
+            var weather1 = await Core.Core.GetWeather();
+            City.Text = weather1.Title;
+
+            var MainTemp = FindViewById<TextView>(Resource.Id.Temp);
+            var weather2 = await Core.Core.GetWeather();
+            MainTemp.Text = weather1.Temperature;
+
+            var WindSpeed = FindViewById<TextView>(Resource.Id.WindSpeed);
+            var weather3 = await Core.Core.GetWeather();
+            WindSpeed.Text = weather3.WindSpeed;
+
+            var MainPressure = FindViewById<TextView>(Resource.Id.MainPressure);
+            var weather4 = await Core.Core.GetWeather();
+            MainPressure.Text = weather3.WindSpeed;
+        }
+    }
+}
